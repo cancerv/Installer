4bdcff6c888927bf8c02507dc71f58616b38ab3c
Veronica <veronicaegorova13@gmail.com>
2022-08-05 16:59:21 +0700

    git

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,109 +3,58 @@ package com.company;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 public class Main {
+    private static final StringBuilder log = new StringBuilder();
 
-    public static final String path = "./Game";
+    public static final String path = Paths.get("Game").toString();
 
     public static void main(String[] args) {
+        makeDir(Paths.get("src").toString());
+        makeDir(Paths.get("src", "main").toString());
+        makeDir(Paths.get("src", "test").toString());
+        makeFile(Paths.get("src", "main").toString(), "Main.java");
+        makeFile(Paths.get("src", "main").toString(), "Utils.java");
+        makeDir(Paths.get("res").toString());
+        makeDir(Paths.get("res", "vectors").toString());
+        makeDir(Paths.get("res", "drawables").toString());
+        makeDir(Paths.get("res", "icons").toString());
+        makeDir(Paths.get("saveGames").toString());
+        makeDir(Paths.get("temp").toString());
+        makeFile(Paths.get("temp").toString(), "temp.txt");
 
-        StringBuilder log = new StringBuilder();
+        flushLog();
+    }
 
-        File dir1 = new File(path + "/src");
-        if (dir1.mkdir()) {
-            log.append("Каталог src создан\n"); //каталог срц
-        } else {
-            log.append("Каталог src не создан\n");
-        }
-
-        File dir2 = new File(path + "/src/main");
-        if (dir2.mkdir()) {
-            log.append("Каталог main создан\n"); //каталог срц
-        } else {
-            log.append("Каталог main не создан\n");
-        } //каталог майн в срц
-
-        File dir3 = new File(path + "/src/test"); //каталог тест в срц
-        if (dir3.mkdir()) {
-            log.append("Каталог test создан\n"); //каталог срц
-        } else {
-            log.append("Каталог test не создан\n");
-        }
-
-
-        File file1 = new File(path + "/src/main", "Main.java"); //файл майл в каталоге майн
+    private static void makeFile(String dir, String name) {
+        File file = new File(Paths.get(path, dir).toString(), name); //файл
         try {
-            if (file1.createNewFile())
-                log.append("Файл Main.java был создан\n");
+            if (file.createNewFile()) {
+                log.append("Файл " + name + " был создан\n");
+            } else {
+                log.append("Файл " + name + " не был создан\n");
+            }
         } catch (IOException ex) {
-            log.append(ex.getMessage());
+            log.append("Файл " + name + " не был создан. Ошибка: " + ex.getMessage() + "\n");
         }
+    }
 
-        File file2 = new File(path + "/src/main", "Utils.java"); //файл утилс в каталоге майн
-        try {
-            if (file2.createNewFile())
-                log.append("Файл Utils.java был создан\n");
-        } catch (IOException ex) {
-            log.append(ex.getMessage());
-        }
-
-        File dir4 = new File(path + "/res");
-        if (dir4.mkdir()) {
-            log.append("Каталог res создан\n"); //каталог рес
+    private static void makeDir(String dirPath) {
+        File dir = new File(Paths.get(path, dirPath).toString()); //создание каталога
+        if (dir.mkdir()) {
+            log.append("Каталог " + dirPath + " создан\n");
         } else {
-            log.append("Каталог res не создан\n");
-        } //создание каталога рес
-
-        File dir5 = new File(path + "/res/vectors");// создание каталога векторс в каталоге рес
-        if (dir5.mkdir()) {
-            log.append("Каталог vectors создан\n");
-        } else {
-            log.append("Каталог vectors не создан\n");
+            log.append("Каталог " + dirPath + " не создан\n");
         }
+    }
 
-        File dir6 = new File(path + "/res/drawables");// создание дровблс векторс в каталоге рес
-        if (dir6.mkdir()) {
-            log.append("Каталог drawables создан\n");
-        } else {
-            log.append("Каталог drawables не создан\n");
-        }
-
-        File dir7 = new File(path + "/res/icons");// создание айконс векторс в каталоге рес
-        if (dir7.mkdir()) {
-            log.append("Каталог icons создан\n");
-        } else {
-            log.append("Каталог icons не создан\n");
-        }
-
-        File dir8 = new File(path + "/savegames"); //создание каталога сейвгейм
-        if (dir8.mkdir()) {
-            log.append("Каталог savegames создан\n");
-        } else {
-            log.append("Каталог savegames не создан\n");
-        }
-
-        File dir9 = new File(path + "/temp"); //создание каталога темп
-        if (dir9.mkdir()) {
-            log.append("Каталог temp создан\n");
-        } else {
-            log.append("Каталог temp не создан\n");
-        }
-
-        File file3 = new File(path + "/temp", "temp.txt"); //файл утилс в каталоге майн
-        try {
-            if (file3.createNewFile())
-                log.append("Файл temp.txt был создан\n");
-        } catch (IOException ex) {
-            log.append(ex.getMessage());
-        }
-
-        try (FileWriter writer = new FileWriter(path + "/temp/temp.txt", false)) {
+    private static void flushLog() {
+        try (FileWriter writer = new FileWriter(Paths.get(path, "temp", "temp.txt").toString(), false)) {
             writer.write(log.toString());
             writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
